20b67fdbc381da9be42918148994d4f65985cdfe
Lucas Gomes <lucasgomesbt@gmail.com>
2024-10-05 00:12:20 -0300

    feat: initial structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+
       },
+      fontFamily: {
+        lufga: ["var(--font-lufga)"],
+        urbanist: ["var(--font-urbanist)"]
+      }
     },
   },
   plugins: [],
