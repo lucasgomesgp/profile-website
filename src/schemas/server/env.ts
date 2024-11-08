236e0703f0fb179af1d4cdb7ea8ea4fabd854ba8
Lucas Gomes <lucasgomesbt@gmail.com>
2024-11-07 22:42:45 -0300

    fix: add restrictions to parse env variables on zod

--- src/schemas/server/env.ts
+++ src/schemas/server/env.ts
@@ -3,15 +3,9 @@ import z from "zod";
 const envVariables = z.object({
     MAIL_USER: z.string().trim().min(1),
     MAIL_PASSWORD: z.string().trim().min(1),
-    MAIL_PORT: z.string(),
-    GRAPHQL_API_URL: z.string().url(),
-    GRAPHQL_TOKEN: z.string(),
+    MAIL_PORT: z.string().min(3),
+    GRAPHQL_API_URL: z.string().url().min(1),
+    GRAPHQL_TOKEN: z.string().min(1),
 });
 
-export const envSchema = envVariables.parse({
-    MAIL_USER: process.env.MAIL_USER,
-    MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-    MAIL_PORT: process.env.MAIL_PORT,
-    GRAPHQL_API_URL: process.env.GRAPHQL_API_URL,
-    GRAPHQL_TOKEN: process.env.GRAPHQL_TOKEN,
-});
+export const envSchema = envVariables.parse(process.env);
